7c6ed8d3000b39e0e91f0dc995489c258ecfa4d8
Marko Horvat <mh3269@student.uni-lj.si>
2022-02-22 13:31:50 +0100

    Dodan izpis pospeška in višine #4

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,6 @@
 public class Gravitacija {
     public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
+        System.out.println(v);
+        System.out.println(a);
     }
 }
