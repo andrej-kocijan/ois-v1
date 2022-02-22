64cafdd127b7c821fe201c67193eed6885635963
Andrej Kocijan <ak5878@student.uni-lj.si>
2022-02-22 13:50:17 +0100

    fixed minor issues

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 public class Gravitacija {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Scanner sc = new Scanner(System.in);
 
-        double C = 6.674*Math.pow(10, -11);
-        double M = 5.972*Math.pow(10, 24);
-        double r = 6.371*Math.pow(10, 6);
+        double C = 6.674 * Math.pow(10, -11);
+        double M = 5.972 * Math.pow(10, 24);
+        double r = 6.371 * Math.pow(10, 6);
 
         double v = sc.nextDouble();
 
         double a = izracun(C, M, r, v);
-=======
+
         System.out.println(v);
         System.out.println(a);
->>>>>>> izpis
+
+        System.out.println("OIS je zakon!");
     }
 
     public static double izracun(double C, double M, double r, double v) {
